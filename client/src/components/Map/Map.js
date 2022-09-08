785221cc1fb623f506f15eeef9753f1189f53aec
Mark Poletaev <mrk.pltv@gmail.com>
2022-09-08 18:54:47 +0300

    geocoder

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Map.css';
 
 const { ymaps } = window;
 
 export default function Map() {
   const center = [55.76, 37.64];
+  const [coordinates, setCoordinates] = useState('');
   function init() {
     const map = new ymaps.Map('map', {
       center, // Москва
@@ -26,6 +27,16 @@ export default function Map() {
 
     map.geoObjects.add(placemark);
   }
+  // const myGeocoder = ymaps.geocode('Бутово');
+  // myGeocoder.then(
+  //   (res) => {
+  //     // alert(`Координаты объекта :${res.geoObjects.get(0).geometry.getCoordinates()}`);
+  //     setCoordinates([res.geoObjects.get(0).geometry.getCoordinates()]);
+  //   },
+  //   (err) => {
+  //     alert('Ошибка');
+  //   },
+  // );
   useEffect(() => {
     ymaps.ready(init);
   }, []);
