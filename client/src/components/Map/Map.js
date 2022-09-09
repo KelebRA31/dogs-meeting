6b79d0a35d81c1804fe58e2182bc246d5b1af24d
Mark Poletaev <mrk.pltv@gmail.com>
2022-09-09 11:51:50 +0300

    added placemarks

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -4,39 +4,48 @@ import './Map.css';
 const { ymaps } = window;
 
 export default function Map() {
-  const center = [55.76, 37.64];
-  const [coordinates, setCoordinates] = useState('');
+  const center = [55.752010565380814, 37.61879118379485];
+  const [map, setMap] = useState(null);
+
   function init() {
-    const map = new ymaps.Map('map', {
-      center, // Москва
+    setMap(new ymaps.Map('map', {
+      center,
       zoom: 10,
     }, {
       searchControlProvider: 'yandex#search',
-    });
-    console.log(coordinates);
-
-    const placemark = new ymaps.Placemark(center, {}, {
-      // hintContent: 'Собственный значок метки',
-      // balloonContent: 'Это красивая метка',
-
-      iconLayout: 'default#image',
-      iconImageHref: 'https://cdn-icons-png.flaticon.com/512/6680/6680947.png',
-      iconImageSize: [40, 40],
-      // // iconImageOffset: [-19, -44],
-
-    });
-
-    map.geoObjects.add(placemark);
+    }));
   }
-  const myGeocoder = ymaps.geocode('Бутово');
-  myGeocoder.then(
-    (res) => {
-      alert(`Координаты объекта :${res.geoObjects.get(0).geometry.getCoordinates()}`);
-    },
-    (err) => {
-      alert('Ошибка');
-    },
-  );
+  console.log(map);
+
+  const placemark = (place) => new ymaps.Placemark(place, {}, {
+
+    iconLayout: 'default#image',
+    iconImageHref: 'https://cdn-icons-png.flaticon.com/512/6680/6680947.png',
+    iconImageSize: [40, 40],
+    iconImageOffset: [-19, -36],
+
+  });
+
+  if (map) {
+    const myGeocoder = ymaps.geocode('Южнобутовская 66');
+    myGeocoder.then(
+      (res) => {
+        const place = placemark(res.geoObjects.get(0).geometry.getCoordinates());
+
+        setMap((prev) => {
+          prev.geoObjects.add(
+            place,
+          );
+          // prev[center] = place;
+          return prev;
+        });
+      },
+      (err) => {
+        alert('Ошибка', err);
+      },
+    );
+  }
+
   useEffect(() => {
     ymaps.ready(init);
   }, []);
