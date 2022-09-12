e086f00bfcccc0d3e426283d90e0ac4ac1152b09
ArtBar9981 <scorp9981@gmail.com>
2022-09-12 10:58:48 +0300

    Carousel added

--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './MainPage.css';
+import Carousel from 'react-bootstrap/Carousel';
 import Map from '../Map/Map';
 
 export default function MainPage() {
   return (
-
     <div>
+      <Carousel className="position-Carousel">
+        <Carousel.Item>
+          <img className="image-carousel" src="/Images/2727570.jpg" alt="pic1" />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="image-carousel" src="/Images/dogswalk.jpeg" width="300px" alt="pic2" />
+          <p>123123</p>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="image-carousel" src="/Images/DogsWalk.png" width="50px" alt="pic3" />
+        </Carousel.Item>
+      </Carousel>
       <Map />
     </div>
 
