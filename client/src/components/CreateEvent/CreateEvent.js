005402db04aee4145f860074be4e2d6db6787fa0
Mark Poletaev <mrk.pltv@gmail.com>
2022-09-14 15:29:09 +0300

    merge

--- client/src/components/CreateEvent/CreateEvent.js
+++ client/src/components/CreateEvent/CreateEvent.js
@@ -243,8 +243,8 @@ export default function CreateEvent({
     <div style={{ width: 'max-content', marginLeft: 'auto' }}>
       {['right'].map((anchor) => (
         <React.Fragment key={anchor}>
-          <button type="button" id="set-balloon-header" className="btn">Задать заголовок балуна</button>
-          <button type="button" id="set-balloon-content" className="btn">Задать содержимое балуна</button>
+          <button type="button" id="set-balloon-header" style={{ display: 'none' }} className="btn">Задать заголовок балуна</button>
+          <button type="button" id="set-balloon-content" style={{ display: 'none' }} className="btn">Задать содержимое балуна</button>
 
           {/* <Button onClick={toggleDrawer(anchor, true)}>
             <ToggleButton value="justify" key="justify">
--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.listen(PORT, () => {
 
       let today = new Date();
       const dd = String(today.getDate()).padStart(2, '0');
-      const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+      const mm = String(today.getMonth() + 1).padStart(2, '0');
       const yyyy = today.getFullYear();
       today = `${yyyy}-${mm}-${dd} ${time}`;
 
